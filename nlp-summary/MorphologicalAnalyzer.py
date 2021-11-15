8f6fcd1b9dd9eaf1b3da80bca08679c30bfc58dc
RyoAiko <94365038+ra1ko@users.noreply.github.com>
2021-11-15 22:00:48 +0900

    Add MorphologicalAnalyzer

--- nlp-summary/MorphologicalAnalyzer.py
+++ nlp-summary/MorphologicalAnalyzer.py
@@ -3,13 +3,21 @@ import MeCab
 # 形態素解析に関するクラス		
 class MorphologicalAnalyzer():
 	def parse(self, str): # MeCabの解析結果を返すメソッド/引数は文字列
+		ps = ParsedSentence()
 		mecab = MeCab.Tagger("-Owakati")
 		parsed = mecab.parse(str).split(' ')
 
-		return parsed
+		for i in range(0,len(parsed)):
+			cw = CalcedWord(parsed[i])
+			ps.setWord(cw)
+
+		return ps.getSentence()
 
 # MeCabによって分解された単語とその重みを表すクラス		
 class CalcedWord():
+	def __init__(self, w):
+		self.weight = 0
+		self.word = w
 	def getWord(self): # 単語ゲッター
 		return self.word
 	def getWeight(self): # 重みゲッター
@@ -17,8 +25,11 @@ class CalcedWord():
 
 # CalcedWordを集めた文とその重み合計を表すクラス
 class ParsedSentence():
+	sent = []
+	def setWord(self, word):
+		self.sent.append(word)
 	def getSentence(self): # 文ゲッター
-		return list
+		return self.sent
 	def getSum(self): # 重みゲッター
 		return sum
 
