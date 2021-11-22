7baf97178458780827fa0479d43db4c65187adc4
tetsustet <50937719+tetsustet@users.noreply.github.com>
2021-11-22 22:24:45 +0900

    modfiy idf_counter.py(wip)

--- nlp-summary/idf_counter.py
+++ nlp-summary/idf_counter.py
@@ -1,5 +1,7 @@
 #!/use/bin/env python3
 import sys
+from pathlib import Path
+
 from MorphologicalAnalyzer import MorphologicalAnalyzer
 
 ma = MorphologicalAnalyzer()
@@ -18,3 +20,12 @@ for sentence in sentence_list:
 with open(sys.argv[1], 'w') as f:
     for k, v in result_dict.items():
         f.write('{},{}\n'.format(k, v))
+
+def make_sentence_list():
+    l = []
+
+if __name__ == "__main__":
+    p = pathlib.Path(sys.argv[1])
+    if(p.exists()):
+        li = [x for x in p.iterdir()]
+    print(li)
