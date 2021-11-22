7dffe4cdec7caf847b119536f1bf877e7b75aef4
tetsustet <50937719+tetsustet@users.noreply.github.com>
2021-11-22 23:04:33 +0900

    modify idf_counter

--- nlp-summary/idf_counter.py
+++ nlp-summary/idf_counter.py
@@ -1,31 +1,42 @@
 #!/use/bin/env python3
 import sys
+import os
 from pathlib import Path
 
 from MorphologicalAnalyzer import MorphologicalAnalyzer
 
 ma = MorphologicalAnalyzer()
 
-result_dict = {}
-sentence_list = ['これはテストの文章です', 'これもテストです']
-for sentence in sentence_list:
-    parsed_sentence = ma.parse(sentence)
-    word_list = map(lambda x: x.getWord(), parsed_sentence.getSentence())
-    word_set = set(word_list)
-    for word in word_set:
-        if word not in result_dict:
-            result_dict[word] = 0
-        result_dict[word] += 1
+def count_idf(dir_name):
+    p = Path(dir_name)
+    if(not p.exists()):
+        return
 
-with open(sys.argv[1], 'w') as f:
-    for k, v in result_dict.items():
-        f.write('{},{}\n'.format(k, v))
+    result_dict = {}
+    sentence_list = make_sentence_list(p)
+    for sentence in sentence_list:
+        parsed_sentence = ma.parse(sentence)
+        word_list = map(lambda x: x.getWord(), parsed_sentence.getSentence())
+        word_set = set(word_list)
+        for word in word_set:
+            if word not in result_dict:
+                result_dict[word] = 0
+            result_dict[word] += 1
 
-def make_sentence_list():
-    l = []
+    with open(sys.argv[1] + "/idf.txt", 'w') as f:
+        for k, v in result_dict.items():
+            f.write('{},{}\n'.format(k, v))
+
+def make_sentence_list(p):
+    l = list()
+    for target in p.glob("*.txt"):
+        print(target)
+        with open(target) as f:
+            lines = f.readlines()
+            # for sentence in lines:
+            #     print(sentence)
+            l += lines
+    return l
 
 if __name__ == "__main__":
-    p = pathlib.Path(sys.argv[1])
-    if(p.exists()):
-        li = [x for x in p.iterdir()]
-    print(li)
+    count_idf(sys.argv[1] + "/")
